35199e7b914432274b5735d191c8b0a58244280d
kwishna <kwishna@gmail.com>
2022-12-28 13:34:27 +0530

    first commit

---
+++ selenium-spring-boot-advanced/src/test/java/com/example/seleniumspring/parallels/passed/Salary.java
@@ -0,0 +1,36 @@
+package com.example.seleniumspring.parallels.passed;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Lazy
+@Component
+@Scope("prototype")
+// @Scope("singleton") // Default scope.
+// Singleton --> JuniorSeniorTests Will Fail For 'singleton', Because Same 'Salary' Bean Object Will Shared To 'JuniorEngineer' & 'SeniorEngineer'.
+// prototype --> JuniorSeniorTests Will Pass For 'prototype', Because Separate 'Salary' Bean Object Will Shared To 'JuniorEngineer' & 'SeniorEngineer'.
+public class Salary {
+
+    Logger logger = LoggerFactory.getLogger(Salary.class);
+    private int amount;
+
+    @Override
+    public String toString() {
+        return "Salary{" +
+                "amount=" + amount +
+                '}';
+    }
+
+    public int getAmount() {
+        logger.info("Getting Salary Amount.");
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        logger.info("Setting Salary Amount: " + amount);
+        this.amount = amount;
+    }
+}
